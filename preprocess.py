52c704d352c814820f381a542a9c5ac02cfad8f7
Javier Maroto <javiermarotomorales@gmail.com>
2025-10-14 16:35:02 +0200

    Add count of supporting Varlociraptor observations and strong observations

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 import matplotlib.pyplot as plt
+import re
 
 
 class Dataloader:
@@ -23,6 +24,10 @@ class Dataloader:
         })
         df_varlociraptor['artifact_reason'] = df_varlociraptor.apply(
             Dataloader.create_artifact_reason_column, axis=1)
+        df_varlociraptor = pd.concat([
+            df_varlociraptor,
+            df_varlociraptor.apply(Dataloader.count_varlociraptor_observations, axis=1)
+        ], axis=1)
         return df_varlociraptor
     
     def load_varlociraptor_normal(self, inpdir):
@@ -55,6 +60,16 @@ class Dataloader:
         )
         df['prob_variant'] = df['PROB_SOMATIC'] + df['PROB_GERMLINE']
         return df
+    
+    def count_varlociraptor_observations(row):
+        res_dict = {}
+        res_dict['CAOBS'] = sum(map(int, re.findall(r'\d+', row['SAOBS'])))
+        res_dict['CROBS'] = sum(map(int, re.findall(r'\d+', row['SROBS'])))
+        res_dict['CSAOBS'] = sum(map(int, re.findall(r'(\d+)(?=[sv])', row['SAOBS'])))
+        res_dict['CSROBS'] = sum(map(int, re.findall(r'(\d+)(?=[sv])', row['SROBS'])))
+        res_dict['ratio_obs'] = res_dict['CAOBS'] / (res_dict['CAOBS'] + res_dict['CROBS'])
+        res_dict['ratio_strong_obs'] = res_dict['CSAOBS'] / (res_dict['CSAOBS'] + res_dict['CSROBS'])
+        return pd.Series(res_dict)
     
     def create_artifact_reason_column(row):
         active_biases = (
@@ -101,8 +116,7 @@ class Dataloader:
             return ['Low MAPQ or major alternative alignment (XA tags)']
         return []
 
-
-def create_scatter_plot_af(df):
+def create_scatter_plot_af(df, af_col='AF'):
     """
     Creates a scatter plot of AF_OBS_VARL (Estimated) vs AF (Observed)
     with a dashed reference line y = x.
@@ -115,7 +129,7 @@ def create_scatter_plot_af(df):
     """
     fig, ax = plt.subplots(figsize=(6, 6))
 
-    ax.scatter(df['AF_OBS_VARL'], df['AF'], alpha=0.7, edgecolor='k')
+    ax.scatter(df['AF_OBS_VARL'], df[af_col], alpha=0.7, edgecolor='k')
     ax.plot([0, 1], [0, 1], '--', color='red', linewidth=1)
 
     ax.set_xlabel('Estimated')
@@ -126,8 +140,7 @@ def create_scatter_plot_af(df):
     ax.grid(True, linestyle='--', alpha=0.6)
 
     return fig
-    
-    
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Load data with Dataloader")
@@ -153,7 +166,7 @@ if __name__ == '__main__':
     dl = Dataloader()
     df = dl.load_varlociraptor(inpdir)
     df_vcf = dl.load_vcf(inpdir)
-    df = df[dl.MERGE_COLS + ['pred', 'prob_variant', 'artifact_reason'] + [col for col in df.columns if col.startswith('PROB_')] + ['AF_VARL', 'DP_VARL', 'SAOBS', 'SROBS']]
+    df = df[dl.MERGE_COLS + ['pred', 'prob_variant', 'artifact_reason'] + [col for col in df.columns if col.startswith('PROB_')] + ['AF_VARL', 'DP_VARL', 'SAOBS', 'SROBS', 'ratio_obs', 'ratio_strong_obs']]
     df = df.merge(df_vcf[dl.MERGE_COLS + ['AF', 'AO', 'FAO', 'DP', 'FDP']], on=dl.MERGE_COLS)
     if os.path.exists(os.path.join(inpdir, "calls_filt_normal.tsv")):
         df = df.merge(dl.load_varlociraptor_normal(inpdir), on=dl.MERGE_COLS)
@@ -163,12 +176,12 @@ if __name__ == '__main__':
     df.loc[(df['pred'] == "SOMATIC") & (df['PROB_GERMLINE'] + 0.05 > df['PROB_SOMATIC']), 'pred'] = 'GERMLINE/SOMATIC'
     df.loc[(df['pred'] == "GERMLINE") & (df['PROB_SOMATIC'] + 0.05 > df['PROB_GERMLINE']), 'pred'] = 'GERMLINE/SOMATIC'
     outdir = os.path.join(params.output_folder, params.id, "results")
-    df.to_csv(os.path.join(outdir, "varlociraptor_probs.tsv"), sep="\t", index=False)
 
     fig = create_scatter_plot_af(df)
-    fig.savefig(os.path.join(outdir, 'estimated_vs_observed_AF.png'), dpi=300, bbox_inches='tight')
-
-    print(df.to_string(index=False))
     n_higher_than_1_estimated_AF = len(df.loc[(df['AF_VARL'] > 0.95) & (df['AF'] > df['AF_OBS_VARL'] * 1.05) & (df['PROB_GERMLINE'] < 0.1)])
+    
+    df.to_csv(os.path.join(outdir, "varlociraptor_probs.tsv"), sep="\t", index=False)
+    fig.savefig(os.path.join(outdir, 'estimated_vs_observed_AF.png'), dpi=300, bbox_inches='tight')
+    print(df.to_string(index=False))
     if n_higher_than_1_estimated_AF > 0:
         print(f'Warning: tumor cell content prior might be too low (based on {n_higher_than_1_estimated_AF} calls)', file=sys.stderr)
